c391e0ef5a14b41b63319026ba4e9c7f7ee2a7a2
Alexis Cesar <alexis.cesar2017@outlook.com>
2022-05-02 19:43:45 -0300

    Removed tile border

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ const drawMap = () => {
                 canvasContext.drawImage(monsterSprite, xCoordinate, yCoordinate, tileWidth, tileHeight);
             }
             
-            canvasContext.stroke();
+            //canvasContext.stroke();
             xCoordinate += tileWidth;
         });
         xCoordinate = 0;
